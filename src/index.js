4abd5354f49373f9bd893b8f2ed78c686b176e40
Alex Zdch <pachtouka@gmail.com>
2020-10-11 18:02:42 +0300

    feat: add reverse-int cycle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+
+    let strNum = n + '';
+    let resultNum = 0;  
+
+    for ( let i = strNum.length - 1; i >= 0; i -- ) {
+
+        resultNum = resultNum + strNum[i] * 10**i;
+    }
+    
+    return resultNum;
 }
